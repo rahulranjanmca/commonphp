07f96bf3748be9524d18619423f7704e9b7a72dd
rahul.ranjan <rahul.ranjan@VLD-016.CORP.VALFORMA.COM>
2016-06-23 18:47:46 +0530

    Committed

---
+++ src/Repositories/RepositoryInterface.php
@@ -0,0 +1,11 @@
+<?php
+interface RepositoryInterface {
+	public function all($columns = array('*'));
+	public function getList($criteria, $perPage = 15, $columns = array('*'));
+	public function save($entity);
+	public function update($entity);
+	public function delete($id);
+	public function getPartialEntity($id, $columns = array('*'));
+	public function getEntityByKeyAndValue($field, $value, $columns = array('*'));
+	public function getVo($field, $value, $columns = array('*'));
+} 
